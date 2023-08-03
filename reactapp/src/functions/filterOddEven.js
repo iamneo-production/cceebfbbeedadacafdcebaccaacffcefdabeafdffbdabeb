9ac28755cc7db781f02a03e2f6534df3826f8426
Iamneo <techadmin@iamneo.ai>
2023-08-03 10:30:39 +0000

    Backup Commit

---
+++ reactapp/src/functions/filterOddEven.js
@@ -0,0 +1,22 @@
+export const filterOdd=(...arr)=>
+{
+    let ans=[];
+    arr.forEach(ele=>{
+        if(ele%2!=0){
+            ans.push(ele);
+        }
+    });
+    return ans;
+}
+export const filterEven=(...arr)=>
+{
+    let ans=[];
+    arr.forEach(ele=>{
+        if(ele%2==0)
+        {
+            ans.push(ele);
+        }
+    });
+    return ans;
+}
+export const generateRandomList
